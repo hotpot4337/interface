b3864442fa354d5101d00a80347d3660078d0da8
KRD <kaimorciniec@gmail.com>
2023-03-25 00:09:17 +0000

    Add account/rpc stores

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -4,13 +4,9 @@ import { derived, writable, type Writable } from 'svelte/store';
 import { ethers } from 'ethers';
 import type { Web3Auth } from '@web3auth/modal';
 import type { UserInfo } from '@web3auth/base';
-
-export const privKey = persisted('privKey', '');
-export const wallet = derived(privKey, ($privKey) => {
-	if ($privKey) {
-		return new Wallet($privKey);
-	}
-});
+import { browser } from '$app/environment';
+import type { SimpleAccountAPI } from '@account-abstraction/sdk/dist/src/SimpleAccountAPI';
+import type { HttpRpcClient } from '@account-abstraction/sdk/dist/src/HttpRpcClient';
 
 export const web3auth: Writable<Web3Auth> = writable();
 export const userData: Writable<Partial<UserInfo> | undefined> = persisted('user', undefined);
@@ -18,4 +14,38 @@ export const provider = derived(web3auth, ($web3auth) => {
 	if (!$web3auth?.provider) return;
 	const provider = new ethers.providers.Web3Provider($web3auth.provider);
 	return provider;
+});
+
+export const accountApi: Writable<SimpleAccountAPI> = writable();
+export const accountAddr: Writable<string> = writable();
+export const rpcClient: Writable<HttpRpcClient> = writable();
+
+web3auth.subscribe(async (web3auth) => {
+	if (!browser) return;
+	if (!web3auth.provider) return;
+	const { SimpleAccountAPI, HttpRpcClient } = await import('@account-abstraction/sdk');
+	const provider = new ethers.providers.Web3Provider(web3auth.provider);
+
+	const config = {
+		rpcUrl: 'http://localhost:8545',
+		signingKey: provider.getSigner(),
+		entryPoint: '0xF4E9c4961D3D24AE4259767E81c9A380aa524298',
+		simpleAccountFactory: '0x15950998f3e5cF176DC0490209552Df5CB68Eb97',
+		bundlerUrl: 'http://localhost:3000/rpc'
+	};
+
+	const accApi = new SimpleAccountAPI({
+		provider,
+		entryPointAddress: config.entryPoint,
+		owner: provider.getSigner(),
+		factoryAddress: config.simpleAccountFactory
+	});
+
+	accountApi.set(accApi);
+
+	const accAddr = await accApi.getCounterFactualAddress();
+	accountAddr.set(accAddr);
+
+	const chainId = await provider.getNetwork().then((net) => net.chainId);
+	rpcClient.set(new HttpRpcClient(config.bundlerUrl, config.entryPoint, chainId));
 });
--- src/routes/wallet/+page.svelte
+++ src/routes/wallet/+page.svelte
@@ -1,86 +1,51 @@
 <script lang="ts">
-	import { privKey, wallet, web3auth, provider, userData } from '$lib/stores';
+	import { web3auth, accountApi, rpcClient, userData, accountAddr, provider } from '$lib/stores';
 	import { Button, Heading } from 'flowbite-svelte';
 	import { ethers, Wallet } from 'ethers';
 	import { onMount } from 'svelte';
-	import type { SimpleAccountAPI } from '@account-abstraction/sdk';
+	import { formatEther } from 'ethers/lib/utils';
 
-	let accountApi: Maybe<SimpleAccountAPI>;
-	let accountAddress: string = '';
-
-	// onMount(init);
-
-	async function init() {
-		console.log('hi');
-		const { SimpleAccountAPI, HttpRpcClient } = await import('@account-abstraction/sdk');
-		const config = {
-			rpcUrl: 'http://localhost:8545',
-			signingKey: $privKey,
-			entryPoint: '0xF4E9c4961D3D24AE4259767E81c9A380aa524298',
-			simpleAccountFactory: '0x15950998f3e5cF176DC0490209552Df5CB68Eb97',
-			bundlerUrl: 'http://localhost:3000/rpc'
-		};
-		if (!$web3auth?.provider) return;
-		await $web3auth.addChain;
-		const provider = new ethers.providers.Web3Provider($web3auth.provider); // web3auth.provider
-
-		accountApi = new SimpleAccountAPI({
-			provider,
-			entryPointAddress: config.entryPoint,
-			owner: await provider.getSigner(),
-			factoryAddress: config.simpleAccountFactory
-		});
-
-		const chainId = await provider.getNetwork().then((net) => net.chainId);
-		const rpcClient = new HttpRpcClient(config.bundlerUrl, config.entryPoint, chainId);
-
+	async function transfer() {
+		if (!$accountApi || !$rpcClient) return;
 		const target = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
-		const value = ethers.utils.parseEther('1.5');
+		const value = ethers.utils.parseEther('1');
 
-		const op = await accountApi.createSignedUserOp({
+		const op = await $accountApi.createSignedUserOp({
 			target,
 			value,
 			data: '0x'
 		});
 
-		const uoHash = await rpcClient.sendUserOpToBundler(op);
+		const uoHash = await $rpcClient.sendUserOpToBundler(op);
 		console.log(`UserOpHash: ${uoHash}`);
 
 		console.log('Waiting for transaction...');
-		const txHash = await accountApi.getUserOpReceipt(uoHash);
+		const txHash = await $accountApi.getUserOpReceipt(uoHash);
 		console.log(`Transaction hash: ${txHash}`);
-		accountAddress = await accountApi.getCounterFactualAddress();
-		console.log(accountAddress);
-	}
-
-	async function getAccountAddress() {
-		if (!accountApi) return;
-		// accountAddress = await walletApi.getCounterFactualAddress();
-	}
-
-	async function transfer() {
-		if (!accountApi) return;
-		// walletApi.createSignedUserOp();
 	}
 </script>
 
 <section class="text-center mt-24">
 	{#if $userData}
-		<Button on:click={() => $web3auth?.logout()} color="red" size="sm">Logout</Button>
-		<Button on:click={getAccountAddress} size="sm">Get address</Button>
 		<div>
 			Your wallet address:
-			{accountAddress}
+			<b>{$accountAddr ?? '---'} </b>
+			<p>
+				{#await $provider?.getBalance($accountAddr)}
+					---
+				{:then value}
+					Balance: {value ? formatEther(value) : '---'} ETH
+				{:catch error}
+					error :(
+				{/await}
+			</p>
 		</div>
-		<Button on:click={init}>Transfer</Button>
+		<p>This wallet must contain eth to execute UserOps (unless a Paymaster is active)</p>
+		<Button on:click={transfer}>Transfer</Button>
 	{:else}
-		<Heading tag="h1" class="text-2xl mb-2">Create wallet</Heading>
-		<p class="max-w-xl m-auto mb-4">
-			Create a magic wallet for <b>gasless transactions</b> and
-			<b>2fa protection</b>! You will see a QR code to scan with Google Authenticator
-		</p>
+		<Heading tag="h1" class="text-xl mb-2">Your magic wallet is one click away</Heading>
+		<p class="max-w-xl m-auto mb-4">Login with any method to access your wallet.</p>
 
-		<!-- <Button on:click={generate} color="dark">Generate wallet 🪄</Button> -->
-		<Button on:click={() => $web3auth.connect()}>Social login</Button>
+		<Button on:click={() => $web3auth.connect()}>Login</Button>
 	{/if}
 </section>
